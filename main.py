3821da400d654aa49af11a7b8465effa5bc26fdd
Tituz175 <tobititus01@gmail.com>
2023-05-08 00:39:58 -0600

    question 1 solution modify

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from square_grid import *
 
 
 def question1():
-    choice = input("Please enter mode (E)ncode or (D)ecode: ")
-    if choice.lower() == "e":
-        text = input("Please enter phrase: ")
-        print(encode("img.png", text))
-    elif choice.lower() == "d":
-        text = decode("output_img.png")
-        print(f"Found: {text}\n")
+    while True:
+        choice = input("Please enter mode (E)ncode or (D)ecode: ")
+        if choice.lower() == "e":
+            text = input("Please enter phrase: ")
+            print(encode("img.png", text))
+        elif choice.lower() == "d":
+            text = decode("output_img.png")
+            print(f"Found: {text}\n")
+        else:
+            break
 
 
 def question2():
@@ -17,7 +20,10 @@ def question2():
 
 
 def main():
+    print("==============Question 1==============")
     question1()
+    print("======================================\n")
+    print("==============Question 2==============\n")
     question2()
 
 
